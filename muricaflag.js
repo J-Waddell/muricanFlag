5b1d1d1c053f970ef123be8b06c2404e4826fe55
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-03-06 09:17:45 -0600

    added const and downloaded modules

---
+++ muricaflag.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+
+const {backRed, backWhite, backBlue, white} = require('chalk')
+
+const stringLength = require('string-length')
+
+const starPad = backBlue("  "),
+      starMarg = backBlue(" "),
+      
